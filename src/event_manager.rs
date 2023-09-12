ca212342afe49aaa790ebf89f851f820464c6a10
Kihau <michal.haldas@gmail.com>
2023-09-12 03:34:00 +0200

    Fix race condition

--- src/event_manager.rs
+++ src/event_manager.rs
@@ -1,48 +1,50 @@
-use std::sync::atomic::{AtomicU32, Ordering};
-use serenity::async_trait;
-use serenity::builder::{ CreateApplicationCommandOption};
-use serenity::client::{Context, EventHandler};
-use serenity::model::application::command::{Command, CommandOptionType};
-use serenity::model::channel::{Message};
-use serenity::model::gateway::Ready;
-use serenity::model::application::interaction::Interaction;
-use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use serenity::model::prelude::application_command::{CommandDataOption, CommandDataOptionValue};
 use rand::Rng;
-use serenity::utils::Color;
+use serenity::{
+    async_trait,
+    builder::CreateApplicationCommandOption,
+    client::{Context, EventHandler},
+    model::{
+        application::{
+            command::{Command, CommandOptionType},
+            interaction::{application_command::ApplicationCommandInteraction, Interaction},
+        },
+        channel::Message,
+        gateway::Ready,
+        prelude::application_command::CommandDataOptionValue
+    },
+    utils::Color, prelude::TypeMapKey,
+};
+use std::sync::{atomic::{Ordering, AtomicUsize}, Arc};
+
+pub struct ConfessionCount;
+impl TypeMapKey for ConfessionCount {
+    type Value = Arc<AtomicUsize>;
+}
 
 pub struct Handler;
-
-impl Handler{
-    pub fn new() -> Self{
-        Self{}
+impl Handler {
+    pub fn new() -> Self {
+        Self
     }
-    fn display_message(msg: Message){
-        if len(&msg.content) == 0 {
-            if msg.attachments.is_empty() {
-                return
-            }
+
+    fn display_message(msg: Message) {
+        if !msg.content.is_empty() {
+            println!("[{}] {}", msg.author.name, msg.content)
+        } else if !msg.attachments.is_empty() {
             println!("[{}] {}", msg.author.name, msg.attachments[0].url);
-            return
-        }
-        println!("[{}] {}", msg.author.name, msg.content)
+        } 
     }
 }
-
-pub fn len(str: &String) -> usize{
-    str.chars().count()
-}
-
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, _ctx: Context, msg: Message) {
         Handler::display_message(msg);
     }
 
     async fn ready(&self, ctx: Context, _ready: Ready) {
         println!("Connected!");
-        ConfessionCommands::register_commands( ctx).await;
+        ConfessionCommands::register_commands(ctx).await;
         println!("Registered Commands!");
     }
 
@@ -51,107 +53,114 @@ impl EventHandler for Handler {
     }
 }
 
-
-// delete(link/msg_id), prefix/help(), reveal
 pub struct ConfessionCommands;
-
-static mut COUNTER: AtomicU32 = AtomicU32::new(1);
-
-impl ConfessionCommands {
-    fn new() -> Self {
-        Self{}
-    }
-}
-
 impl ConfessionCommands {
     pub async fn register_commands(context: Context) {
-        let mut text_option = CreateApplicationCommandOption::default();
-        text_option
+        let text_option = CreateApplicationCommandOption::default()
             .name("text")
             .description("confession")
             .kind(CommandOptionType::String)
-            .required(true);
-        let mut attachment_option = CreateApplicationCommandOption::default();
-        attachment_option
-                .name("attachment")
-                .description("A file")
-                .kind(CommandOptionType::Attachment)
-                .required(false);
-        let confess_command = Command::create_global_application_command(&context.http, |command| {
-            command.name("confess").description("Confess your sins anonymously")
+            .required(true)
+            .to_owned();
+
+        let attachment_option = CreateApplicationCommandOption::default()
+            .name("attachment")
+            .description("A file")
+            .kind(CommandOptionType::Attachment)
+            .required(false)
+            .to_owned();
+
+        Command::create_global_application_command(&context.http, |command| {
+            command
+                .name("confess")
+                .description("Confess your sins anonymously")
                 .add_option(text_option)
                 .add_option(attachment_option)
-        }).await;
-        confess_command.expect("Unable to register the confess command");
+        }).await.expect("Unable to register the confess command");
 
+        let message_id_option = CreateApplicationCommandOption::default()
+            .name("message_id")
+            .description("Message id of the confession")
+            .kind(CommandOptionType::String)
+            .required(true)
+            .to_owned();
 
-        let mut message_id_option = CreateApplicationCommandOption::default();
-        message_id_option
-                .name("message_id")
-                .description("Message id of the confession")
-                .kind(CommandOptionType::String)
-                .required(true);
-
-        let delete_command = Command::create_global_application_command(&context.http, |command| {
-            command.name("delete").description("Delete your thought silently")
+        Command::create_global_application_command(&context.http, |command| {
+            command
+                .name("delete")
+                .description("Delete your thought silently")
                 .add_option(message_id_option)
-        }).await;
-        delete_command.expect("Unable to register the delete command");
+        }).await.expect("Unable to register the delete command");
 
-
-        let mut user_option = CreateApplicationCommandOption::default();
-        user_option
+        let user_option = CreateApplicationCommandOption::default()
             .name("user")
             .description("User to be reported")
             .kind(CommandOptionType::User)
-            .required(true);
+            .required(true)
+            .to_owned();
 
-        let mut reason_option = CreateApplicationCommandOption::default();
-        reason_option
+        let reason_option = CreateApplicationCommandOption::default()
             .name("reason")
             .description("What did they do?")
             .kind(CommandOptionType::String)
-            .required(false);
-        let report_command = Command::create_global_application_command(&context.http, |command| {
-            command.name("report").description("Report user")
+            .required(false)
+            .to_owned();
+
+        Command::create_global_application_command(&context.http, |command| {
+            command
+                .name("report")
+                .description("Report user")
                 .add_option(user_option)
                 .add_option(reason_option)
-        }).await;
-        report_command.expect("Unable to register the report command");
+        }).await.expect("Unable to register the report command");
     }
 
-
-    pub async fn confess(context: Context, command: ApplicationCommandInteraction){
+    pub async fn confess(context: Context, command: ApplicationCommandInteraction) {
         println!("Confess command from: {}", command.user.name);
         println!("Options len: {}", command.data.options.len());
-        let text_option = &command.data.options[0].as_val();
 
-        command.create_interaction_response(&context.http, |interaction|{
+        let options = &command.data.options;
+        let text_option = &options[0].resolved;
+
+        command.create_interaction_response(&context.http, |interaction| {
             interaction.interaction_response_data(|data| {
                 data.ephemeral(true).content(":white_check_mark: Your confession has been added!")
             });
             interaction
-        }).await.expect("unable to interact");
-        let CommandDataOptionValue::String(text) = text_option else {
+        }).await.expect("Unable to interact.");
+
+        let Some(CommandDataOptionValue::String(text)) = text_option else {
             return;
         };
+
+        let confession_count = {
+            let data = context.data.read().await;
+            let counter = data.get::<ConfessionCount>()
+                .expect("Failed to get ConfessionCount")
+                .clone();
+
+            counter.fetch_add(1, Ordering::Relaxed)
+        };
+
         command.channel_id.send_message(&context.http, |msg| {
             let mut rng = rand::thread_rng();
             let r = rng.gen_range(0..256) as u8;
             let g = rng.gen_range(0..256) as u8;
             let b = rng.gen_range(0..256) as u8;
 
-            msg.embed(|embed| unsafe {
-                embed.title(format!("Anonymous Confession (#{})", COUNTER.fetch_add(1, Ordering::Relaxed)))
+            msg.embed(|embed| {
+                embed
+                    .title(format!("Anonymous Confession (#{})", confession_count))
                     .description(text)
                     .footer(|footer| {
                         footer.text("❗ If this confession is ToS-breaking or overtly hateful, you can report it using \"/report\"")
                     })
                     .color(Color::from_rgb(r, g, b));
+
                 if command.data.options.len() > 1 {
-                    let attach_option = command.data.options[1].as_val();
-                    if let CommandDataOptionValue::Attachment(attachment) = attach_option {
-                        println!("url:{}", attachment.url);
+                    let attach_option = &options[1].resolved;
+                    if let Some(CommandDataOptionValue::Attachment(attachment)) = attach_option {
+                        println!("url: {}", attachment.url);
                         embed.image(&attachment.url);
                     }
                 }
@@ -159,20 +168,23 @@ impl ConfessionCommands {
             })
         }).await.expect("Failed to send");
     }
-    pub async fn delete(context: Context, command: ApplicationCommandInteraction){
+
+    pub async fn delete(_context: Context, command: ApplicationCommandInteraction) {
         println!("Delete command from: {}", command.user.name);
     }
 
     pub async fn report(context: Context, command: ApplicationCommandInteraction) {
-        let mut content = String::from(command.user.name);
-        content.push_str(" tried to report a confession");
+        let name = command.user.name;
+        let content = format!("{name} tried to report a confession");
 
-        command.channel_id.send_message(&context.http, |msg|{
-            msg.content(content)
-        }).await.expect("not sent");
+        command
+            .channel_id
+            .send_message(&context.http, |msg| msg.content(content))
+            .await
+            .expect("Message wasd not sent.");
     }
 
-    pub async fn resolve_interaction(context: Context, interaction: Interaction){
+    pub async fn resolve_interaction(context: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             println!("Command: {:?}", command);
             match command.data.name.as_str() {
@@ -181,28 +193,6 @@ impl ConfessionCommands {
                 "report"  => ConfessionCommands::report(context, command).await,
                 _ => {}
             }
-        }
-    }
-}
-
-trait ToOptionValue{
-    fn as_val(&self) -> &CommandDataOptionValue;
-}
-
-impl ToOptionValue for CommandDataOption{
-    fn as_val(&self) -> &CommandDataOptionValue{
-        self.resolved.as_ref().unwrap()
-    }
-}
-
-trait Read{
-    fn read(&self) -> u32;
-}
-
-impl Read for AtomicU32{
-    fn read(&self) -> u32 {
-        unsafe{
-            self.load(Ordering::Relaxed)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
 mod event_manager;
 
+use event_manager::{ConfessionCount, Handler};
 use serenity::prelude::*;
-use crate::event_manager::Handler;
+use std::sync::{atomic::AtomicUsize, Arc};
 
-const TOKEN: &str = "N1t4A6d94gCO5jj80Rfx6kHC.f5MA7u.L37lny8VREf7mqgzz28XftaeDBk";
+const TOKEN: &str = "Ask to Ask";
+// (/.,.                                             
+//  *##.,**.                                 ///..*&&
+// /,(#%(.,.                                 **%&&%/,
+// ** (##%&(.***(///..../* /*///,   .**** *%&&&&*.***
+// ***,(##%&&&%&&&&&&&&&@&&&&&&&%#/#%&&&&&%%%%(.*    
+// ** (%&&&&%%%%&&&%%%%%%%%%%%%%%%%%%%###(((*. **    
+// ( #&&&&%%###((((######((((//**//(###%#(/.,        
+// .#%&%%(*..,//(((####(((//*,,,. .,..,(%%(./        
+// %&&%%#*,...,,(%%%%%%###(/**,,..,,*/(((##(         
+// %%%%%%%%%%%%%%%%%%%%%###########((((((((#(.***    
+// ############(##%%%###((###%%%######((//((((***    
+// #(////((((((//////***(((((##((((((((/////(((/,..,.
+// /(/***/((((((///*****/((((((///////,,**///(//// ..
+//  /(((#(*,,///*,*//////((((#%##%%/**/(((((((((///(.
+//  ,##(((#(,###%(/((/(%%%#%&&&&%///((((((((((((////(
+// ** (##(((/,(%&&%%&&%&&&&&@&%///((((((((((((///////
+//      /##(/*,,(##&&&%&&&((*///((((((((((/////////((
+//     */,((((/**,,,,...,**///((((((//////////////(((
+//     .,,.(###((//****//((((((((////////////////((((
 
 #[tokio::main]
 async fn main() {
-
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(TOKEN, intents)
         .event_handler(Handler::new())
         .await
         .expect("Error creating client");
-    // start listening for events in less than 2500 servers
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
-        return;
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ConfessionCount>(Arc::new(AtomicUsize::new(0)));
     }
 
+    // Start listening for events in less than 2500 servers.
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
